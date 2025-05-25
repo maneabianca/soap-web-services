2cb7b22afd09424a0310251b91383515803f4e45
Bianca <manea.bianca9@yahoo.ro>
2025-05-25 13:49:49 +0300

    Adding the GetAllCourseDetails request and response

---
+++ src/main/java/com/soap/webservices/soapcoursemanagement/soap/service/CourseDetailsService.java
@@ -0,0 +1,58 @@
+package com.soap.webservices.soapcoursemanagement.soap.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.soap.webservices.soapcoursemanagement.soap.bean.Course;
+
+@Component
+public class CourseDetailsService {
+
+	//create them in memory
+	private static List<Course> courses = new ArrayList<>();
+	
+	static {
+		Course firstCourse = new Course(1, "Spring", "10 Steps");
+		courses.add(firstCourse);
+		Course secondCourse = new Course(2, "Spring MVC", "10 Examples");
+		courses.add(secondCourse);
+		Course thirdCourse = new Course(3, "Spring Boot", "6k Students");
+		courses.add(thirdCourse);
+		Course fourthCourse = new Course(4, "Maven", "OK");
+		courses.add(fourthCourse);
+		
+	}
+	
+	// Get the details for one course
+	public Course findById(int id) {
+		for(Course course: courses) {
+			if(course.getId() == id) {
+				return course;
+			}
+		}
+		return null;
+	}
+	
+	// Get all courses
+	public List<Course> findAll(){
+		return courses;
+	}
+	
+	// Delete a course
+	public int deleteById(int id) {	
+		Iterator<Course>  iterator = courses.iterator();
+		while(iterator.hasNext()) {
+			Course course = iterator.next();
+			if(course.getId() == id) {
+				iterator.remove();
+				return 1;
+			}
+		}
+		return 0;
+	}
+	// updating course and new course
+	
+}
